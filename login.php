d2eb667529df365862b4c6c8a172bda4b7d57b52
enricorosano <79046538+enricorosano@users.noreply.github.com>
2021-07-22 11:08:27 +0800

    Add files via upload

--- login.php
+++ login.php
@@ -1,76 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-body, html {
-  overflow: hidden;
-  height: 100%;
-  font-family: Arial, Helvetica, sans-serif;
-}
-
-* {
-  box-sizing: border-box;
-}
-
-.bg-img {
-  /* The image used */
-  background-image: url("123.jpeg");
-
-  min-height: 400px;
-
-  /* Center and scale the image nicely */
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  position: relative;
-}
-
-/* Add styles to the form container */
-.container {
-  position: absolute;
-  right: 0;
-  margin: 20px;
-  max-width: 300px;
-  padding: 16px;
-  background-color: white;
-}
-
-/* Full-width input fields */
-input[type=text], input[type=password] {
-  width: 100%;
-  padding: 15px;
-  margin: 5px 0 22px 0;
-  border: none;
-  background: #f1f1f1;
-}
-
-input[type=text]:focus, input[type=password]:focus {
-  background-color: #ddd;
-  outline: none;
-}
-
-/* Set a style for the submit button */
-.btn {
-  background-color: #04AA6D;
-  color: white;
-  padding: 16px 20px;
-  border: none;
-  cursor: pointer;
-  width: 100%;
-  opacity: 0.9;
-}
-
-.btn:hover {
-  opacity: 1;
-}
-</style>
+    <meta charset="utf-8"/>
+    <title>PASAKAY LOGIN</title>
+    <link rel="stylesheet" href="s.css">
+    
 </head>
 <body>
-
-<div class="bg-img">
 <?php
-    require('dbconnection.php');
+    require('database.php');
     session_start();
     // When form submitted, check and create user session.
     if (isset($_POST['username'])) {
@@ -79,36 +17,47 @@ input[type=text]:focus, input[type=password]:focus {
         $password = stripslashes($_REQUEST['password']);
         $password = mysqli_real_escape_string($con, $password);
         // Check user is exist in the database
-        $query    = "SELECT * FROM `admin` WHERE Username='$username'
-                     AND Password='" . md5($password) . "'";
+        $query    = "SELECT Username,Password FROM `admin` WHERE Username='$username'
+                     AND Password='$password'";
         $result = mysqli_query($con, $query) or die(mysql_error());
         $rows = mysqli_num_rows($result);
         if ($rows == 1) {
             $_SESSION['username'] = $username;
             // Redirect to user dashboard page
-            header("Location: homepage.php");
+            header("Location:dashboard.php");
         } else {
             echo "<div class='form'>
-                  <h3>Incorrect Username/password.</h3><br/>
+                  <h3>You have input incorrect Username/Password.</h3><br/>
                   <p class='link'>Click here to <a href='login.php'>Login</a> again.</p>
                   </div>";
         }
     } else {
 ?>
-    <form method="post" class="container">
-    <h1>Login</h1>
 
-    <label for="email"><b>Email</b></label>
-    <input type="text" placeholder="Enter Username" name="username" required>
 
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="password" required>
 
-    <button type="submit" class="btn">Login</button>
+
+    <form class="form" method="post" name="login">
+    <h1 class="login-title"><b style="color: white">PASAKAY</b></h1>
+        <input type="text" class="login-input" name="username" placeholder="Username" autofocus="true" required />
+        <input type="password" class="login-input" name="password" placeholder="Password"/ required>
+        <input type="submit" value="Login" name="submit" class="login-button"/><br> <br>
+        <label>
+        <input type="checkbox" checked="checked" name="remember"> Remember me
+      </label>
+        <p class="link"><a style="color: white" href="index.php">Back to Homepage</a></p>
   </form>
 <?php
     }
 ?>
-
+<div style="color: white" id="footer">
+        <p align="center" style="background-color: brown">&copy; PASAKAY
+        <?php
+            getdate();
+            $mydate=getdate(date("U"));
+            echo "$mydate[month] $mydate[mday], $mydate[year]";
+            ?>
+            </p>
+    </div
 </body>
 </html>
